fefa80b665c8dab53dda987c174c1e83a8463ba0
mdeejay <mdjrussia@gmail.com>
2019-09-01 07:29:58 +0200

    vendor: Add 720p bootanimation
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -16,7 +16,9 @@
 
 TARGET_BOOT_ANIMATION_RES ?= undefined
 
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/beast/prebuilt/common/media/720.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/beast/prebuilt/common/media/1080.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
      PRODUCT_COPY_FILES += vendor/beast/prebuilt/common/media/2160.zip:system/media/bootanimation.zip
